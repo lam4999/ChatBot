59c6fe52565fc427092411b6887cccf5cb5d1b66
lam4999 <lamdtph49959@gmail.com>
2025-08-17 15:20:45 +0700

    code

--- db.js
+++ db.js
@@ -1,1 +1,12 @@
 const mysql = require('mysql12');
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "chatbot_db",
+});
+db.connect(err=>{
+    if(err) throw err;
+    console.log("✅ MySQL connected!");
+});
+module.exports = db;
